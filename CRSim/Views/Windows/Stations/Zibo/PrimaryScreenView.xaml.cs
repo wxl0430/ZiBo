a147df8c9e29968616837c6312e7129dccfb2173
wxl0430 <141288415+wxl0430@users.noreply.github.com>
2025-03-02 15:02:38 +0800

    Add files via upload

---
+++ CRSim/Views/Windows/Stations/Zibo/PrimaryScreenView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using CRSim.ViewModels.Zibo;
+
+namespace CRSim.Views.Zibo
+{
+    /// <summary>
+    /// PrimaryScreen.xaml 的交互逻辑
+    /// </summary>
+    public partial class PrimaryScreenView : Window
+    {
+        public PrimaryScreenViewModel ViewModel { get; }
+        public PrimaryScreenView(PrimaryScreenViewModel viewModel)
+        {
+            InitializeComponent();
+            ViewModel = viewModel;
+            DataContext = viewModel;
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Close();
+            }
+        }
+    }
+}
