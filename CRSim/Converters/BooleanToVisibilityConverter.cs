d6f7c020e620ab4603eb0e1ee0c2222ea26494e3
denglihong2007 <2639367181@qq.com>
2025-03-06 00:15:25 +0800

    style: 改善参数设置体验

---
+++ CRSim/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿namespace CRSim.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+            {
+                return b ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
