a60a7b4405f245d9837b8e9caf45e0969a36281c
wxl0430 <141288415+wxl0430@users.noreply.github.com>
2025-04-10 20:30:17 +0800

    添加了淄博站站台看板

--- CRSim.ScreenSimulator/Converters/LocationToStringConverter.cs
+++ CRSim.ScreenSimulator/Converters/LocationToStringConverter.cs
@@ -5,23 +5,44 @@ namespace CRSim.ScreenSimulator.Converters
 {
     public class LocationToStringConverter : IMultiValueConverter
     {
+        public string DisplayMode { get; set; } = "normal";
         object IMultiValueConverter.Convert(object[] values, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            if (values[0] is int Length&& values[1] is int Location)
+            if (values[0] is int Length && values[1] is int Location)
             {
-                if (Location > Length)
+                if (DisplayMode == "less")
                 {
-                    return $"当前无车，1-{Length}车向后。";
+                    if (Location > Length)
+                    {
+                        return $"当前无车厢，1~{Length}车厢向前。";
+                    }
+                    if (Location == Length)
+                    {
+                        return $"当前{Length}车厢，1车厢向前。";
+                    }
+                    if (Location == 1)
+                    {
+                        return $"当前1车厢，{Length}车厢向后。";
+                    }
+                    return $"1车厢向前，{Length}车厢向后。";
                 }
-                if (Location == Length)
+                if (DisplayMode == "normal")
                 {
-                    return $"当前{Length}车，1-{Length-1}车向后。";
+                    if (Location > Length)
+                    {
+                        return $"当前无车，1-{Length}车向后。";
+                    }
+                    if (Location == Length)
+                    {
+                        return $"当前{Length}车，1-{Length-1}车向后。";
+                    }
+                    if (Location == 1)
+                    {
+                        return $"当前1车，2-{Length}车向前。";
+                    }
+                    return $"当前{Location}车，1-{Location - 1}车向前，{Location + 1}-{Length}车向后。";
                 }
-                if (Location == 1)
-                {
-                    return $"当前1车，2-{Length}车向前。";
-                }
-                return $"当前{Location}车，1-{Location - 1}车向前，{Location + 1}-{Length}车向后。";
+                return string.Empty;
             }
             return string.Empty;
         }
