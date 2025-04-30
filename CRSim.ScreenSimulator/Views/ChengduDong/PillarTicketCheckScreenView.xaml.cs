7761d08528d1b65831f9db167118a20fce409a63
denglihong2007 <98096191+denglihong2007@users.noreply.github.com>
2025-05-01 02:12:43 +0800

    feat: 新增成都东站检票口立柱看板

---
+++ CRSim.ScreenSimulator/Views/ChengduDong/PillarTicketCheckScreenView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using CRSim.ScreenSimulator.ViewModels.ChengduDong;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace CRSim.ScreenSimulator.Views.ChengduDong
+{
+    /// <summary>
+    /// SecondaryScreen.xaml 的交互逻辑
+    /// </summary>
+    public partial class PillarTicketCheckScreenView : Window
+    {
+        public PillarTicketCheckScreenViewModel ViewModel { get; }
+        public PillarTicketCheckScreenView(PillarTicketCheckScreenViewModel viewModel)
+        {
+            InitializeComponent();
+            ViewModel = viewModel;
+            RenderOptions.SetEdgeMode(this, EdgeMode.Aliased);
+            DataContext = this;
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Close();
+            }
+        }
+    }
+}
