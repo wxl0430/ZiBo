b54d2fd5960b56a5ed6bafd4da74d4008c7afa7f
denglihong2007 <98096191+denglihong2007@users.noreply.github.com>
2025-05-14 23:25:03 +0800

    feat: #140 添加柳州站主要看板

---
+++ CRSim.ScreenSimulator/ViewModels/Niuzhou/PrimaryScreenViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CRSim.Core.Models;
+using CRSim.Core.Services;
+using CRSim.ScreenSimulator.Models;
+using System.Collections.ObjectModel;
+using System.Windows;
+namespace CRSim.ScreenSimulator.ViewModels.Niuzhou
+{
+    public class PrimaryScreenViewModel : ScreenViewModel
+    {
+        public ObservableCollection<TrainInfo> LeftScreen { get; private set; } = [];
+        public PrimaryScreenViewModel(ITimeService timeService, ISettingsService settingsService)
+            : base(timeService, settingsService)
+        {
+            Text = $"开车前{settingsService.GetSettings().PassingCheckInAdvanceDuration.TotalMinutes}分钟开始检票，开车前{settingsService.GetSettings().StopCheckInAdvanceDuration.TotalMinutes}分钟停止检票。";
+            ItemsPerPage = 10;
+            PageCount = 1;
+            StationType = StationType.Departure;
+            timeService.RefreshSecondsElapsed += RefreshDisplay;
+            Initialize();
+        }
+        private async void Initialize()
+        {
+            await WaitForDataLoadAsync();
+            RefreshDisplay(null,null);
+        }
+        private void RefreshDisplay(object? sender, EventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                int pageCount = (int)Math.Ceiling((double)TrainInfo.Count / (ItemsPerPage * PageCount));
+                LeftScreen.Clear();
+                int startIndex = CurrentPageIndex * ItemsPerPage * PageCount;
+                var leftItems = TrainInfo.Skip(startIndex).Take(ItemsPerPage).ToList();
+                while (leftItems.Count < ItemsPerPage) // Fill up with new object() if not enough items
+                {
+                    leftItems.Add(new TrainInfo());
+                }
+                foreach (var item in leftItems)
+                {
+                    LeftScreen.Add(item);
+                }
+                CurrentPageIndex = CurrentPageIndex + 1 >= Math.Min(_settings.MaxPages, pageCount) ? 0 : CurrentPageIndex + 1;
+            });
+        }
+    }
+}
