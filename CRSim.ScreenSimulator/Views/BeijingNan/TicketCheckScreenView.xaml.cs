7c989c6ec861be11ee958950c75a08b9d2733027
wxl0430 <141288415+wxl0430@users.noreply.github.com>
2025-05-23 14:26:37 +0800

    feat: #122 #124 添加北京南站检票口看板和站台次要看板

---
+++ CRSim.ScreenSimulator/Views/BeijingNan/TicketCheckScreenView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using CRSim.ScreenSimulator.ViewModels.BeijingNan;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+namespace CRSim.ScreenSimulator.Views.BeijingNan
+{
+    /// <summary>
+    /// SecondaryScreen.xaml 的交互逻辑
+    /// </summary>
+    public partial class TicketCheckScreenView : BaseScreenView
+    {
+        public TicketCheckScreenViewModel ViewModel { get; }
+        public TicketCheckScreenView(TicketCheckScreenViewModel viewModel)
+        {
+            InitializeComponent();
+            ViewModel = viewModel;
+            DataContext = viewModel;
+        }
+    }
+}
