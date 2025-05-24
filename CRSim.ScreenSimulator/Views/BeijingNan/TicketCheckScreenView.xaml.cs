cfb9753097d48f37651051eadfac0de9d603e216
denglihong2007 <98096191+denglihong2007@users.noreply.github.com>
2025-05-24 20:03:30 +0800

    feat: 完善简化代码 [skip cliff]

--- CRSim.ScreenSimulator/Views/BeijingNan/TicketCheckScreenView.xaml.cs
+++ CRSim.ScreenSimulator/Views/BeijingNan/TicketCheckScreenView.xaml.cs
@@ -1,12 +1,6 @@
 ﻿using CRSim.ScreenSimulator.ViewModels.BeijingNan;
-using System.Windows;
-using System.Windows.Input;
-using System.Windows.Media;
 namespace CRSim.ScreenSimulator.Views.BeijingNan
 {
-    /// <summary>
-    /// SecondaryScreen.xaml 的交互逻辑
-    /// </summary>
     public partial class TicketCheckScreenView : BaseScreenView
     {
         public TicketCheckScreenViewModel ViewModel { get; }
