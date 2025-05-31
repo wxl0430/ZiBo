e949fc55cd1d71f07549c4af14bbefaa1fa59b1c
denglihong2007 <98096191+denglihong2007@users.noreply.github.com>
2025-05-31 23:21:27 +0800

    feat: #123 添加北京南站站台主要看板

---
+++ CRSim.ScreenSimulator/Converters/ChineseToPinyinConverter.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.International.Converters.PinYinConverter;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace CRSim.ScreenSimulator.Converters
+{
+    public class ChineseToPinyinConverter : IValueConverter
+    {
+        /// <summary>
+        /// 将中文字符串转换为拼音（带声调或不带声调）
+        /// </summary>
+        /// <param name="value">要转换的中文字符串</param>
+        /// <returns>拼音字符串</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return string.Empty;
+            string chineseText = value.ToString();
+            if (string.IsNullOrWhiteSpace(chineseText))
+                return string.Empty;
+
+            var sb = new StringBuilder();
+            foreach (char c in chineseText)
+            {
+                if (ChineseChar.IsValidChar(c))
+                {
+                    var chineseChar = new ChineseChar(c);
+                    var pinyins = chineseChar.Pinyins;
+                    if (pinyins != null && pinyins.Count > 0)
+                    {
+                        var pinyin = pinyins[0][..^1];
+                        sb.Append(pinyin);
+                    }
+                    else
+                    {
+                        sb.Append(c);
+                    }
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+            var result = sb.ToString().ToLower();
+            return char.ToUpper(result[0], culture) + result[1..];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
