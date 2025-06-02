dc97560e517affe17a82e398ff9e94c0a0271319
wxl0430 <141288415+wxl0430@users.noreply.github.com>
2025-06-02 15:35:48 +0800

    feat: #125 添加了德州东站主要看板

---
+++ CRSim.ScreenSimulator/Views/DezhouDong/PrimaryScreenView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using CRSim.ScreenSimulator.ViewModels.DezhouDong;
+namespace CRSim.ScreenSimulator.Views.DezhouDong
+{
+    public partial class PrimaryScreenView : BaseScreenView
+    {
+        public PrimaryScreenViewModel ViewModel { get; }
+        public PrimaryScreenView(PrimaryScreenViewModel viewModel)
+        {
+            InitializeComponent();
+            ViewModel = viewModel;
+            DataContext = viewModel;
+        }
+    }
+}
