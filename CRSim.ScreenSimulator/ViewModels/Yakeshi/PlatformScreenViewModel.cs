2e4014bac114b6552480ac0c4ded6488a70b265b
wxl0430 <141288415+wxl0430@users.noreply.github.com>
2025-06-22 16:14:02 +0800

    feat: #183 #188 添加了海拉尔站主要看板和牙克石站站台看板

---
+++ CRSim.ScreenSimulator/ViewModels/Yakeshi/PlatformScreenViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CRSim.Core.Services;
+namespace CRSim.ScreenSimulator.ViewModels.Yakeshi
+{
+    public class PlatformScreenViewModel : ScreenViewModel
+    {
+        public PlatformScreenViewModel(ITimeService timeService, ISettingsService settingsService)
+            : base(timeService, settingsService)
+        {
+            Text = $"上下车不要拥挤，小心列车与站台之间的缝隙，看管好老人和同行的儿童";
+            ItemsPerPage = 1;
+        }
+    }
+}
---
+++ CRSim.ScreenSimulator/Views/Hailaer/PrimaryScreenView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using CRSim.ScreenSimulator.ViewModels.Hailaer;
+namespace CRSim.ScreenSimulator.Views.Hailaer
+{
+    public partial class PrimaryScreenView : BaseScreenView
+    {
+        public PrimaryScreenViewModel ViewModel { get; }
+        public PrimaryScreenView(PrimaryScreenViewModel viewModel)
+        {
+            InitializeComponent();
+            ViewModel = viewModel;
+            DataContext = viewModel;
+        }
+    }
+}
